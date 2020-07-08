d4fe13be272c93044d73c8f916d5a997d5834c12
Rafael Garcia <rafagrcb@gmail.com>
2020-07-08 16:38:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,17 +16,17 @@ def play
 end
 
   def turn
-    puts "Player #{current_player}, please enter a number 1-9:"
+    puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    cp = current_player
+    char = current_player
     if valid_move?(index)
-      move(index, cp)
+      move(index, char)
       display_board
     else
       turn
     end
-  end
+end
 
   def input_to_index(input)
     input.to_i - 1
